474952c3450afa920387bb1db56099fdf22049c4
rezanikafarin <rezanikafarin100@gmail.com>
2024-05-31 12:57:40 +0430

    create header and install sass

---
+++ src/components/header/Header.js
@@ -0,0 +1,12 @@
+import { Component } from "react";
+import './header.css';
+
+class header extends Component{
+    render(){
+        return(
+            <div className="header">Header</div>
+        )
+    }
+}
+
+export default header;
