49f7c0e00733f310d5374d407d9b3ab70923c917
rezanikafarin <rezanikafarin100@gmail.com>
2024-05-31 14:01:44 +0430

    change header.css to header.scss

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import Header from './components/header/Header'
+import Header from './components/header/Header';
 import './App.css';
 
 function App() {
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,5 +1,5 @@
 import { Component } from "react";
-import './header.css';
+import "header.scss";
 
 class header extends Component{
     render(){
