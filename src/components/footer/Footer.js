5756be779355195b33769dcb9dd9aa8b2a79f9c6
rezanikafarin <rezanikafarin100@gmail.com>
2024-05-31 23:14:48 +0430

    create footer component

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,14 @@
+import {Component} from 'react';
+import './footer.scss';
+
+class Footer extends Component{
+    render(){
+        return(
+            <div className='footer'>
+                Footer
+            </div>
+        )
+    }
+}
+
+export default Footer;
