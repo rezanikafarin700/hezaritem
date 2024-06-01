163f639bd98ecaeac7e922e417ff3647f96b14df
rezanikafarin <rezanikafarin100@gmail.com>
2024-06-01 18:11:45 +0430

    create sidebar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import Header from './components/header/Header';
 import Footer from './components/footer/Footer';
+import Sidebar from './components/sidebar/Sidebar';
 import './App.scss';
 
 function App() {
   return (
     <div className="app">
       <Header/>
-      <div className='content'>content</div>
+      <div className='content'>
+          <Sidebar/>
+      </div>
       <Footer/>
     </div>
   );
---
+++ src/components/sidebar/Sidebar.js
@@ -0,0 +1,12 @@
+import {Component} from 'react';
+import './sidebar.scss';
+
+class Sidebar extends Component{
+    render(){
+        return(
+            <div className='sidebar'>Sidebar</div>
+        )
+    }
+}
+
+export default Sidebar;
