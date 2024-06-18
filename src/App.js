eb948688fbb6d2d9ba0028cd7adcdf8ea881813a
rezanikafarin <rezanikafarin100@gmail.com>
2024-06-18 18:45:28 +0430

    any change for test

--- src/App.js
+++ src/App.js
@@ -1,47 +1,47 @@
-import { Header,Footer,Main } from './components';
-import { useEffect,useState } from 'react';
-import axios from 'axios';
+import { Header, Footer, Main } from "./components";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
-import './App.scss';
+import "./App.scss";
 
-const  App = () => {
-
-  const [loading,setLoading] = useState(false);
-  const [getProducts,setProducts] = useState([]);
-  const [getUsers , setUsers] = useState([]);
+const App = () => {
+  const [loading, setLoading] = useState(false);
+  const [getProducts, setProducts] = useState([]);
+  const [getUsers, setUsers] = useState([]);
 
   useEffect(() => {
     console.log("useEffect...");
     const featchData = async () => {
-      try{
+      try {
         setLoading(true);
-        const {data : products} = await axios.get('http://localhost:9000/products');
-        const {data : users} = await axios.get('http://localhost:9000/users');
+        const { data: products } = await axios.get(
+          "http://localhost:9000/products"
+        );
+        const { data: users } = await axios.get("http://localhost:9000/users");
         setProducts(products);
         setUsers(users);
         setLoading(false);
 
-        console.log('products = ',products);
-        console.log('users = ',users);
-      }
-      catch(err){
+        console.log("products = ", products);
+        console.log("users = ", users);
+      } catch (err) {
         console.log(err.message);
         setLoading(false);
       }
-    }
+    };
 
     featchData();
-  },[]);
+  }, []);
 
-  console.log("getProducts = ",getProducts);
+  console.log("getProducts = ", getProducts);
 
   return (
     <div className="app">
-      <Header/> 
-      <Main data={getProducts}/>
-      <Footer/>
+      <Header />
+      <Main data={getProducts} />
+      <Footer />
     </div>
   );
-}
+};
 
 export default App;
