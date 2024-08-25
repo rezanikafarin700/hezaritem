b3509e7b3b50448e29f93f0c4affc15dca67b543
rezanikafarin <rezanikafarin100@gmail.com>
2024-08-25 10:35:29 +0330

    This component is mostly used in native and domestic projects, and for other projects, it should be done according to the relevant api agen

--- src/components/test-infinit-loading/TestInfinitLoading.jsx
+++ src/components/test-infinit-loading/TestInfinitLoading.jsx
@@ -2,20 +2,18 @@ import InfinitLoading from "../infinit-loading-component/InfinitLoading";
 import "./test-infinit-loading.scss";
 
 const TestInfinitLoading = () => {
-  // const BaseURL = "https://jsonplaceholder.typicode.com/todos";
-  const BaseURL =
-    "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=demo";
+  const BaseURL = "https://jsonplaceholder.typicode.com/todos";
   // const BaseURL = "https://gorest.co.in/public/v2/users";
   const config = {};
 
   return (
     <>
-      <InfinitLoading BaseURL={BaseURL} config={config} responseDataField='Time Series'>
+      <InfinitLoading BaseURL={BaseURL} config={config} responseDataField={null}>
         <div className="container">
-          {/* <h2 className="container__title">title</h2>
+          <h2 className="container__title">title</h2>
           <p className="container__user">userId</p>
           <p className="container__id">id</p>
-          <i className="container__completed">completed</i> */}
+          <i className="container__completed">completed</i>
 
           {/* <div>id</div>
           <div>name</div>
@@ -23,11 +21,6 @@ const TestInfinitLoading = () => {
           <div>gender</div>
           <div>status</div> */}
 
-          <div>open</div>
-          <div>high</div>
-          <div>low</div>
-          <div>close</div>
-          <div>volume</div>
         </div>
       </InfinitLoading>
     </>
